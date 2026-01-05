5437fa38189e003bc2ecb14a2d7d9ecb81612a09
Mizll <ngoquocminh03hh3@gmail.com>
2026-01-05 11:30:12 +0700

    update ui admin

--- obj/Debug/net8.0/BTL_LTWNC.AssemblyInfo.cs
+++ obj/Debug/net8.0/BTL_LTWNC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BTL_LTWNC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d3c3cb2ddc966d9864648daec665ac0239e0c1d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5760a6ded3a5b3ae7a4ff4ee245761f9b4e99741")]
 [assembly: System.Reflection.AssemblyProductAttribute("BTL_LTWNC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BTL_LTWNC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
